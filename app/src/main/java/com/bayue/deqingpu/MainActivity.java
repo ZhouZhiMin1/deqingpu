1e73d05f5b4ae07b39ca53b601c403e23539bf5b
ZhouZhiMin1 <min98788@163.com>
2017-06-02 00:35:48 +0800

    33333

---
+++ app/src/main/java/com/bayue/deqingpu/MainActivity.java
@@ -0,0 +1,34 @@
+package com.bayue.deqingpu;
+
+import android.support.v4.view.ViewCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.bayue.deqingpu.base.BaseActivity;
+
+import static com.zhy.autolayout.utils.ScreenUtils.getStatusBarHeight;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        ViewGroup mContentView = (ViewGroup)findViewById(Window.ID_ANDROID_CONTENT);
+        View statusBarView = mContentView.getChildAt(0);
+        //移除假的 View
+        if (statusBarView != null && statusBarView.getLayoutParams() != null && statusBarView.getLayoutParams().height ==getStatusBarHeight(this)) {
+            mContentView.removeView(statusBarView);
+        }
+        //不预留空间
+        if (mContentView.getChildAt(0) != null) {
+            ViewCompat.setFitsSystemWindows(mContentView.getChildAt(0), false);
+        }
+        setContentView(R.layout.geren_activity_quanzi);
+    }
+}
---
+++ app/src/main/java/com/bayue/deqingpu/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.bayue.deqingpu.base;
+
+import android.app.Activity;
+
+import com.zhy.autolayout.AutoLayoutActivity;
+
+/**
+ * Created by Administrator on 2017/5/22.
+ */
+
+public class BaseActivity extends AutoLayoutActivity {
+}
